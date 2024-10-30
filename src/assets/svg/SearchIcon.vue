32acc23f00a5643b5de2fba04b983cae82c8a652
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-30 20:59:39 +0330

    feat: svg icons added and converted to vue component

---
+++ src/assets/svg/SearchIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    width="800px"
+    height="800px"
+    viewBox="0 0 21 21"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g
+      fill="none"
+      fill-rule="evenodd"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      transform="translate(3 3)"
+    >
+      <circle cx="5.5" cy="5.5" r="5" />
+
+      <line x1="14.5" x2="9.076" y1="14.5" y2="9.076" />
+    </g>
+  </svg>
+</template>
