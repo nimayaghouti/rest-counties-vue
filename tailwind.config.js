e7492977c4c91ef5f853611a34b22676c3cc80a0
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-30 21:40:46 +0330

    setup: container and dark mode added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '375px',
+        lg: '1440px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Nunito Sans', 'sans-serif'],
