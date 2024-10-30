7def4ee3b9319ba0a25582b7e0d928317358d5d6
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-10-30 22:01:41 +0330

    feat: color pallete added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ export default {
       fontFamily: {
         sans: ['Nunito Sans', 'sans-serif'],
       },
+      colors: {
+        'dark-blue': '#1A2631', // Dark Blue (Dark Mode Elements)
+        'dark-blue-bg': '#161E24', // Very Dark Blue (Dark Mode Background)
+        'dark-blue-text': '#1A1A1A', // Very Dark Blue (Light Mode Text)
+        'dark-gray-input': '#7F7F7F', // Dark Gray (Light Mode Input)
+        'light-gray-bg': '#F7F7F7', // Very Light Gray (Light Mode Background)
+        white: '#FFFFFF', // White (Dark Mode Text & Light Mode Elements)
+      },
     },
   },
   plugins: [],
